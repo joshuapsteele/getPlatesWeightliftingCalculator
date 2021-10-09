b98ef28490f56b90a25a811b45140e70b7966932
Joshua Steele <joshua.p.steele@gmail.com>
2021-10-09 10:27:40 -0400

    Look, Ma, my first Python code!

--- main.py
+++ main.py
@@ -64,20 +64,4 @@ while True:
         else:
             print("Invalid input! Please try again with a number that's "
                   "(1) greater than 45 and (2) evenly divisible by 5!")
-
-# # This is a sample Python script.
-#
-# # Press Shift+F10 to execute it or replace it with your code.
-# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            
